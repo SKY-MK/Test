d281e18c1ef35be688996a0a86aca329ad54a50d
SKY-MN <yshashi000@gmail.com>
2022-06-15 11:18:10 +0530

    Adding Employee.java

---
+++ Employee.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public final class Employee {
+    private final Integer id;
+    private final List<String> name;
+
+    public Employee(Integer id, List<String> name){
+        this.id = id;
+        this.name = name;
+        this.name = name;
+        }
+
+
+}
+
+
+
+
